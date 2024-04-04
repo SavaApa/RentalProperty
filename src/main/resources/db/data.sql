9d7b1860e9e3578db5dd7d51bf786fa2b0424c2b
SavaApa <2005savaapanasenko@gmail.com>
2024-04-04 23:35:58 +0200

    Initial commit

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,53 +1,54 @@
 INSERT INTO landlords (l_id, num_property, rented_out, num_free)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 3, 2, 1),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 2, 1, 1);
+VALUES (UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', '')), 3, 2, 1),
+       (UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')), 2, 1, 1);
 
 INSERT INTO tenants (t_id, preference_district, preference_num_room, preference_max_rent, pet_friendly,
                      parking_required, preference_property)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'Downtown', 2, 1500.00, true, false, 'Apartment'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'Suburbia', 3, 2000.00, false, true, 'House');
+VALUES (UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', '')), 'Downtown', 2, 1500.00, true, false, 'Apartment'),
+       (UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')), 'Suburbia', 3, 2000.00, false, true, 'House');
 
 INSERT INTO properties (prop_id, address, price, type_property, num_rooms, district, parking_space, pets, landlord_id,
                         tenant_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', '123 Main St, Downtown', 1200.00, 'APARTMENT', 2, 'Downtown', true, false, '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', '456 Elm St, Suburbia', 1800.00, 'PRIVATE_HOUSE', 3, 'Suburbia', true, true, 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), '123 Main St, Downtown', 1200.00, 'APARTMENT', 2, 'Downtown', true, false, UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), '456 Elm St, Suburbia', 1800.00, 'PRIVATE_HOUSE', 3, 'Suburbia', true, true, UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')));
 
 INSERT INTO applications (ap_id, began_date, application_status, landlord_id, property_id, tenant_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', '2024-03-15', 'ACCEPTED', '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', '2024-03-20', 'REJECTED', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), '2024-03-15', 'ACCEPTED', UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891ddd4045f19844149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), '2024-03-20', 'REJECTED', UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989fb3546968c654a7d141c23e1', '-', '')));
 
 INSERT INTO contracts (c_id, start_date, end_date, application_id, property_id, tenant_id, landlord_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', '2024-04-01', '2025-03-31', '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', '2024-04-05', '2025-04-04', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), '2024-04-01', '2025-03-31', UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), '2024-04-05', '2025-04-04', UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')));
 
 INSERT INTO payments (pay_id, amount, payment_date, payment_status, contract_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 1200.00, '2024-04-01', 'PAID', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 1800.00, '2024-04-05', 'PAID', 'fdd80989-fb35-4696-8c65-4a7d141c23e1');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), 1200.00, '2024-04-01', 'PAID', UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), 1800.00, '2024-04-05', 'PAID', UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')));
 
 INSERT INTO user_info (ui_id, email, password)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'landlord1@example.com', 'password123'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'tenant1@example.com', 'password456');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), 'landlord1@example.com', 'password123'),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), 'tenant1@example.com', 'password456');
 
 INSERT INTO users (u_id, first_name, last_name, user_info_id, tenant_id, landlord_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'John', 'Doe', '5481891d-dd40-45f1-9844-149e01f0c5bb', NULL, '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'Jane', 'Smith', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1', NULL);
-
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), 'John', 'Doe', UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), NULL, UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), 'Jane', 'Smith', UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), NULL);
 
 INSERT INTO roles (r_id, role_name)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'Landlord'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'Tenant');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), 'Landlord'),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), 'Tenant');
 
 INSERT INTO authorities (aut_id, authority_name)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'READ'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'WRITE');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), 'READ'),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), 'WRITE');
 
 INSERT INTO role_authority (role_id, authority_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('5481891d-dd40-45f1-9844-149e01f0c5bb', 'fdd80989-fb35-4696-8c65-4a7d141c23e1'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', '5481891d-dd40-45f1-9844-149e01f0c5bb');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')));
 
 INSERT INTO user_role (user_id, role_id)
-VALUES ('5481891d-dd40-45f1-9844-149e01f0c5bb', '5481891d-dd40-45f1-9844-149e01f0c5bb'),
-       ('fdd80989-fb35-4696-8c65-4a7d141c23e1', 'fdd80989-fb35-4696-8c65-4a7d141c23e1');
+VALUES (UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', '')), UNHEX(REPLACE('5481891d-dd40-45f1-9844-149e01f0c5bb', '-', ''))),
+       (UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')), UNHEX(REPLACE('fdd80989-fb35-4696-8c65-4a7d141c23e1', '-', '')));
+
+
 
 
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS landlords
 (
-    l_id         CHAR(36) PRIMARY KEY,
+    l_id         BINARY(16) PRIMARY KEY,
     num_property INT NOT NULL,
     rented_out   INT NOT NULL,
     num_free     INT NOT NULL
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS landlords
 
 CREATE TABLE IF NOT EXISTS tenants
 (
-    t_id                CHAR(36) PRIMARY KEY,
+    t_id                BINARY(16) PRIMARY KEY,
     preference_district VARCHAR(255) NOT NULL,
     preference_num_room INT          NOT NULL,
     preference_max_rent DECIMAL      NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS tenants
 
 create table properties
 (
-    prop_id       CHAR(36) PRIMARY KEY,
+    prop_id       BINARY(16) PRIMARY KEY,
     address       VARCHAR(255) NOT NULL,
     price         DECIMAL      NOT NULL,
     type_property VARCHAR(255) NOT NULL,
@@ -27,8 +27,8 @@ create table properties
     district      VARCHAR(255) NOT NULL,
     parking_space BOOLEAN      NOT NULL,
     pets          BOOLEAN      NOT NULL,
-    landlord_id   CHAR(36)     NOT NULL,
-    tenant_id     CHAR(36)     NOT NULL,
+    landlord_id   BINARY(16)   NOT NULL,
+    tenant_id     BINARY(16)   NOT NULL,
     FOREIGN KEY (landlord_id) REFERENCES landlords (l_id),
     FOREIGN KEY (tenant_id) REFERENCES tenants (t_id)
 );
@@ -37,12 +37,12 @@ create table properties
 
 CREATE TABLE IF NOT EXISTS applications
 (
-    ap_id              CHAR(36) PRIMARY KEY,
+    ap_id              BINARY(16) PRIMARY KEY,
     began_date         DATE         NOT NULL,
     application_status VARCHAR(255) NOT NULL,
-    landlord_id        CHAR(36)     NOT NULL,
-    property_id        CHAR(36)     NOT NULL,
-    tenant_id          CHAR(36)     NOT NULL,
+    landlord_id        BINARY(16)   NOT NULL,
+    property_id        BINARY(16)   NOT NULL,
+    tenant_id          BINARY(16)   NOT NULL,
     FOREIGN KEY (landlord_id) REFERENCES landlords (l_id),
     FOREIGN KEY (property_id) REFERENCES properties (prop_id),
     FOREIGN KEY (tenant_id) REFERENCES tenants (t_id)
@@ -50,13 +50,13 @@ CREATE TABLE IF NOT EXISTS applications
 
 CREATE TABLE IF NOT EXISTS contracts
 (
-    c_id           CHAR(36) PRIMARY KEY,
-    start_date     DATE     NOT NULL,
-    end_date       DATE     NOT NULL,
-    application_id CHAR(36) NOT NULL,
-    property_id    CHAR(36) NOT NULL,
-    tenant_id      CHAR(36) NOT NULL,
-    landlord_id    CHAR(36) NOT NULL,
+    c_id           BINARY(16) PRIMARY KEY,
+    start_date     DATE       NOT NULL,
+    end_date       DATE       NOT NULL,
+    application_id BINARY(16) NOT NULL,
+    property_id    BINARY(16) NOT NULL,
+    tenant_id      BINARY(16) NOT NULL,
+    landlord_id    BINARY(16) NOT NULL,
     FOREIGN KEY (application_id) REFERENCES applications (ap_id),
     FOREIGN KEY (property_id) REFERENCES properties (prop_id),
     FOREIGN KEY (tenant_id) REFERENCES tenants (t_id),
@@ -65,29 +65,29 @@ CREATE TABLE IF NOT EXISTS contracts
 
 CREATE TABLE IF NOT EXISTS payments
 (
-    pay_id         CHAR(36) PRIMARY KEY,
+    pay_id         BINARY(16) PRIMARY KEY,
     amount         DECIMAL      NOT NULL,
     payment_date   DATE         NOT NULL,
     payment_status VARCHAR(255) NOT NULL,
-    contract_id    CHAR(36)     NOT NULL,
+    contract_id    BINARY(16)   NOT NULL,
     FOREIGN KEY (contract_id) REFERENCES contracts (c_id)
 );
 
 CREATE TABLE IF NOT EXISTS user_info
 (
-    ui_id    CHAR(36) PRIMARY KEY,
+    ui_id    BINARY(16) PRIMARY KEY,
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    u_id         CHAR(36) PRIMARY KEY,
+    u_id         BINARY(16) PRIMARY KEY,
     first_name   VARCHAR(255) NOT NULL,
     last_name    VARCHAR(255) NOT NULL,
-    user_info_id CHAR(36)     NOT NULL,
-    tenant_id    CHAR(36),
-    landlord_id  CHAR(36),
+    user_info_id BINARY(16)   NOT NULL,
+    tenant_id    BINARY(16),
+    landlord_id  BINARY(16),
     FOREIGN KEY (user_info_id) REFERENCES user_info (ui_id),
     FOREIGN KEY (tenant_id) REFERENCES tenants (t_id),
     FOREIGN KEY (landlord_id) REFERENCES landlords (l_id)
@@ -95,28 +95,28 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS roles
 (
-    r_id      CHAR(36) PRIMARY KEY,
+    r_id      BINARY(16) PRIMARY KEY,
     role_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS authorities
 (
-    aut_id         CHAR(36) PRIMARY KEY,
+    aut_id         BINARY(16) PRIMARY KEY,
     authority_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS role_authority
 (
-    role_id      CHAR(36) REFERENCES roles (r_id),
-    authority_id char(36) REFERENCES authorities (aut_id),
+    role_id      BINARY(16) REFERENCES roles (r_id),
+    authority_id BINARY(16) REFERENCES authorities (aut_id),
     PRIMARY KEY (role_id, authority_id),
     FOREIGN KEY (role_id) REFERENCES roles (r_id)
 );
 
 CREATE TABLE IF NOT EXISTS user_role
 (
-    user_id CHAR(36) NOT NULL,
-    role_id CHAR(36) NOT NULL,
+    user_id BINARY(16) NOT NULL,
+    role_id BINARY(16) NOT NULL,
     PRIMARY KEY (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES users (u_id),
     FOREIGN KEY (role_id) REFERENCES roles (r_id)
