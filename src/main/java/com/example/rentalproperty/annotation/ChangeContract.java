4e19f9a8c0b8219e476ed1c03abe2eedfeaa5f0f
SavaApa <2005savaapanasenko@gmail.com>
2024-05-03 16:27:04 +0200

    Initial commit

--- src/main/java/com/example/rentalproperty/annotation/ChangeContract.java
+++ src/main/java/com/example/rentalproperty/annotation/ChangeContract.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @RequestMapping(method = RequestMethod.PUT)
